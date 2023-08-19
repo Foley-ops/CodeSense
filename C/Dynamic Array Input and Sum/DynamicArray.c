a71df3b2a6bca65b6c0602436fd73d67e862d1f6
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-08-18 22:36:51 -0500

    Dynamic array

--- C/Dynamic Array Input and Sum/DynamicArray.c
+++ C/Dynamic Array Input and Sum/DynamicArray.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+
+    int *ptr;
+    int askInput;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &askInput);
+
+    ptr = (int*)malloc(askInput * sizeof (int));
+
+    printf("%d", *ptr);
 
     return 0;
 }
